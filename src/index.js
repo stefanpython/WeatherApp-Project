db78188d721519c4c44b90620d3a3cbe84eadacb
stefanpython <stefanhtml@yahoo.com>
2023-01-02 18:22:42 +0200

    Write async function to retrieve and process news api data

--- src/index.js
+++ src/index.js
@@ -1,26 +1,116 @@
 async function getWeather() {
-  const response = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=Bucharest&APPID=62ab457233b8c68e94ef44e9b7be882d&units=metric"
-  );
-  const weatherData = await response.json();
+  try {
+    const response = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=Bucharest&APPID=62ab457233b8c68e94ef44e9b7be882d&units=metric"
+    );
+    const weatherData = await response.json();
 
-  console.log(weatherData);
+    return weatherData;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-getWeather();
+function aData() {
+  getWeather().then((data) => {
+    console.log(data);
+    // Temperatures
+    console.log(data.main);
+    // data.main.temp
+    // data.main.feels_like
+    // data.main.temp_min
+    // data.main.temp_max
+    // data.main.pressure
+    // data.main.humidity
 
-// function getWeatherPromise() {
-//   const fetchWeather = fetch(
-//     "https://api.openweathermap.org/data/2.5/weather?q=Bucharest&APPID=62ab457233b8c68e94ef44e9b7be882d&units=metric"
-//   );
+    // Name
+    console.log(data.name);
 
-//   fetchWeather
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (response) {
-//       console.log(response);
-//     });
-// }
+    // Sys
+    console.log(data.sys);
+    // data.sys.country
+    // data.sys.sunrise - convert to new Date()
+    // data.sys.sunset - convert
 
-// getWeatherPromise();
+    // Sky status
+    console.log(data.weather[0]);
+    // data.weather[0].description
+
+    // Wind
+    console.log(data.wind);
+    // data.wind.speed
+    // data.wind.deg
+
+    // Coordonates
+    console.log(data.coord);
+    // data.coord.lon
+    // data.coord.lat
+  });
+}
+aData();
+
+// Temperatures
+function tempData() {
+  getWeather().then((data) => {
+    // data.main.temp
+    // data.main.feels_like
+    // data.main.temp_min
+    // data.main.temp_max
+    // data.main.pressure
+    // data.main.humidity
+  });
+}
+tempData();
+
+// City name
+function cityNameData() {
+  getWeather((data) => {
+    // data.name
+  });
+}
+cityNameData();
+
+// Country Sunrise Sunset
+function countryData() {
+  getWeather().then((data) => {
+    // data.sys.country
+    // data.sys.sunrise - convert to new Date()
+    // data.sys.sunset - convert
+  });
+}
+countryData();
+
+// Sky description
+function skyData() {
+  getWeather().then((data) => {
+    // data.weather[0].description
+  });
+}
+skyData();
+
+// Wind data
+function windData() {
+  getWeather().then((data) => {
+    // data.wind.speed
+    // data.wind.deg
+  });
+}
+
+windData();
+
+// Coordonates data
+function coordonatesData() {
+  getWeather().then((data) => {
+    // data.coord.lon
+    // data.coord.lat
+  });
+}
+coordonatesData();
+
+function iconData() {
+  getWeather().then((data) => {
+    // data.weather[0].icon
+  });
+}
+
+iconData();
